d30495ee43c28af3962964b189bd35650b31a564
Eunchul Jung <agfe09@gmail.com>
2010-05-10 22:37:50 +0900

    update: cyworld api key config

--- fabooclub/config/environment.rb
+++ fabooclub/config/environment.rb
@@ -3,3 +3,5 @@ require File.expand_path('../application', __FILE__)
 
 # Initialize the rails application
 Fabooclub::Application.initialize!
+
+CYWORLD_CONFIG = YAML.load_file("#{Rails.root}/config/cyworld.yml")
